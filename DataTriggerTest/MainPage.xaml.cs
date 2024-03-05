8a3c5fe17b28d423b927257899a8111d64ba0123
Alex Rousseaux <alexr@peacocks.com.au>
2024-03-05 13:02:52 +1100

    Add project files.

---
+++ DataTriggerTest/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace DataTriggerTest
+{
+    public partial class MainPage : ContentPage
+    {
+        private MainViewModel _viewModel;
+
+        public MainPage(MainViewModel mainViewModel)
+        {
+            InitializeComponent();
+            _viewModel = mainViewModel;
+            BindingContext = _viewModel;
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+
+            _viewModel.IsPortrait = height > width;
+        }
+    }
+}
